96b654c91c3adb3670906d50e9b21876d0ab1f0a
Igor Marcelli <igor.marcelli@gmail.com>
2024-02-05 09:41:17 -0300

    alterações para github

--- db.php
+++ db.php
@@ -1,17 +1,3 @@
 <?php
-$server = "DESKTOP-VO0N48H";
-$username = "Igor";
-$password = "1234";
-$database = "projeto_spot";
-
-$conn = sqlsrv_connect($server, array(
-    "Database" => $database,
-    "Uid" => $username,
-    "PWD" => $password,
-    "CharacterSet" => "UTF-8"
-));
-
-if (!$conn) {
-    die(print_r(sqlsrv_errors(), true));
-}
+include 'config.php';
 ?>
